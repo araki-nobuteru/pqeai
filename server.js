b99546473ad08936518d8918e495569e50b7ca1f
Flavio Nobuteru Tachikawa Araki <f.tachikawa.araki@avanade.com>
2017-11-17 23:42:52 -0200

    Just a test

---
+++ server.js
@@ -0,0 +1,32 @@
+// "use sctrict"
+
+const bunyan = require("bunyan");
+const restify = require("restify");
+
+const DEFAULT_PORT = 9095;
+const serverOptions = {
+    name: "pqeai-server"
+};
+
+let server = restify.createServer(serverOptions);
+
+// server.use(restify.plugins.CORS());
+server.use(restify.plugins.jsonBodyParser());
+
+server.post("teste/post", (req, res, next) => {
+    let a = req.body;
+
+    res.send(200, a);
+
+    next();
+});
+
+server.get("/teste/foo", (req, res, next) => {
+    res.send(200, {
+        a: "hello!"
+    });
+
+    next();
+});
+
+server.listen(DEFAULT_PORT);
